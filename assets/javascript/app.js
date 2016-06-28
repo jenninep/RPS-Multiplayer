4205657c971e20b08839cbf41d45b259d6049781
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-06-28 18:51:26 -0400

    what i have so far

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,31 +5,47 @@
     databaseURL: "https://rps-multiplayer-cd7d7.firebaseio.com",
     storageBucket: "rps-multiplayer-cd7d7.appspot.com",
   };
-  firebase.initializeApp(config);
-  var database = firebase.database();
+firebase.initializeApp(config);
+var database = firebase.database().ref();
 
 
-// Global variables
-var player1 = ""
-var player2 = ""
-var player1Pick = ""
-var player2Pick
+database.child('player1').set({
+playerName: '',
+playerWins: '',
+playerLosses: '',
+playerChoice: ''
+
+})
+
   
 
+var obj = {
+	choice: "",
+	hasPlayed: false,
+}
+
+
+
+//player picks a choice 
+//	player fires pick function
+// check to see if opponent has made a choice
+// if opponent hasnt picked "display, waiting for you"
 
 
   $("#scissors").on('click', function(){
-  var result = compare('scissors', player2());
+  	obj.hasPlayed = true;
+  	obj.choice = "scissors";
+  var result = compare('scissors', player2);
   $("#decision").html(result);
 });
 
 $('#rock').on('click', function(){
-  var result = compare('rock', player2());
+  var result = compare('rock', player2);
   $("#decision").html(result);
 });
 
 $('#paper').on('click', function(){
-	var result = compare('paper', player2());
+	var result = compare('paper', player2);
 	$('#decision').html(result);
 });
 
@@ -65,8 +81,9 @@ var compare = function(player1, player2){
 $('#playerName').on('submit', function () {
 			var firstName = $('#firstName').val();
 			console.log(firstName)
-			database.ref().text(firstName);
-			$('#playerName').append(firstName);
+			database.set(firstName);
+			$('#P1').html(firstName);
+			return false;
 			// $('#playerName').text('#P1')
 		});
 
